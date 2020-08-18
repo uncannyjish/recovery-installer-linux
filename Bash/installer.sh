86e0da494464f8fb44be4375e1d83cd819e78f99
uncannyjish <43221561+uncannyjish@users.noreply.github.com>
2020-08-19 01:33:31 +0530

    Finalized Version 0.6

--- Bash/installer.sh
+++ Bash/installer.sh
@@ -3,7 +3,12 @@
 printf "\nDetecting your device[Make sure it is in fastboot mode{VOL DN+Power}]...\n\n"
 
 "`dirname $0`/../platform-tools/fastboot" getvar product 2>&1 | grep "^product: *$1$"
-if [ $? -ne 0 ] ; then echo "Device mismatch"; exit 1; fi
+if [ $? -ne 0 ] ; then echo "Device mismatch: $1"; exit 1; fi
+
+if [ $1 == whyred ]
+"`dirname $0`/../platform-tools/fastboot" flash antirbpass "`dirname $0`/../images/dummy_whyred.img"
+if [ $? -ne 0 ] ; then echo "Flash dummy error"; exit 1; fi
+fi
 
 "`dirname $0`/../platform-tools/fastboot" flash recovery "`dirname $0`/../images/$1.img"
 if [ $? -ne 0 ] ; then echo "Flash recovery error"; exit 1; fi
--- start.sh
+++ start.sh
@@ -2,18 +2,25 @@
 
 clear
 printf "\n======================================"
-printf "\n TWRP Installer 0.3 by @uncannyjish"
+printf "\n TWRP Installer 0.6 by @uncannyjish"
 printf "\n======================================\n\n"
 
 choices=("Install Recovery"
 	 "Fix System Destroyed"
 	 "Exit")
 	 
-devices=("Mido"
+devices=("Santoni"
+	 "Mido"
+	 "Vince"
+	 "Whyred"
+	 "Tulip"
+	 "Lavender"
+	 "Violet"
 	 "Ginkgo"
-	 "Santoni"
 	 "Willow"
+	 "Davinci"
 	 "Raphael"
+	 "Beryllium"
 	 "Return to Main Menu")
 	 
 fix=("Ginkgo"
@@ -25,56 +32,40 @@ while [ "$M" != 0 ]; do
 	printf "\nWhat do you want to do?\n\n"
 	select C in "${choices[@]}"; do
 		case "$C" in 
-			"Install Recovery" ) printf "\n\n" 
+			"Install Recovery") printf "\n\n" 
 				select D in "${devices[@]}" ; do
 				case "$D" in 
-					"Mido" ) `dirname $0`/Bash/installer.sh mido
+					Santoni|Mido|Vince|Whyred|Tulip|Lavender|Violet|Ginkgo|Willow|Davinci|Raphael|Beryllium) $SHELL "`dirname $0`/Bash/installer.sh" "${D,,}"
 					clear
 					break 2
 					;;
-					"Ginkgo" ) `dirname $0`/Bash/installer.sh ginkgo
-					clear
-					break 2
+					"Return to Main Menu") break 2
 					;;
-					"Santoni" ) `dirname $0`/Bash/installer.sh santoni
-					clear
-					break 2
-					;;
-					"Willow" ) `dirname $0`/Bash/installer.sh willow
-					clear
-					break 2
-					;;
-					"Raphael" ) `dirname $0`/Bash/installer.sh raphael
-					clear
-					break 2
-					;;
-					"Return to Main Menu" ) break 2
-					;;
-					* ) printf "\n\nCan't you read?\n\n"
+					*) printf "\n\nCan't you read?\n\n"
 					break
 					;;
 				esac
 				done
 				;;
-			"Fix System Destroyed" ) printf "\n\n"
+			"Fix System Destroyed") printf "\n\n"
 				select D in "${fix[@]}" ; do
 				case "$D" in 
-					"Ginkgo" ) `dirname $0`/Bash/fix.sh ginkgo
+					Ginkgo) $SHELL "`dirname $0`/Bash/fix.sh" "${D,,}"
 					clear break 2
 					;;
-					"Return to Main Menu" ) break 2
+					"Return to Main Menu") break 2
 					;;
-					* ) printf "\n\nCan't you read?\n\n"
+					*) printf "\n\nCan't you read?\n\n"
 					break
 					;;
 				esac
 				done
 				;;
-			"Exit" ) printf "\n\nExiting....\n\n" 
+			"Exit") printf "\n\nExiting....\n\n" 
 				M=1
 				break 2
 				;;
-			* ) printf "\n\nCan't you read?\n\n"
+			*) printf "\n\nCan't you read?\n\n"
 				break
 				;;
 		esac
