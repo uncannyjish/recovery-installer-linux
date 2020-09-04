030074015a950cfc81c79935bfea1e7ac28571e1
uncannyjish <43221561+uncannyjish@users.noreply.github.com>
2020-09-04 22:47:56 +0530

    Fixed if statements

--- Bash/installer.sh
+++ Bash/installer.sh
@@ -5,10 +5,9 @@ printf "\nDetecting your device[Make sure it is in fastboot mode{VOL DN+Power}].
 "`dirname $0`/../platform-tools/fastboot" getvar product 2>&1 | grep "^product: *$1$"
 if [ $? -ne 0 ] ; then echo "Device mismatch: $1"; exit 1; fi
 
-if [ $1 == whyred ]
-then
-"`dirname $0`/../platform-tools/fastboot" flash antirbpass "`dirname $0`/../images/dummy_whyred.img"
-if [ $? -ne 0 ] ; then echo "Flash dummy error"; exit 1; fi
+if [ $1 == whyred ] ; then 
+	"`dirname $0`/../platform-tools/fastboot" flash antirbpass "`dirname $0`/../images/dummy_whyred.img"
+	if [ $? -ne 0 ] ; then echo "Flash dummy error"; exit 1; fi
 fi
 
 "`dirname $0`/../platform-tools/fastboot" flash recovery "`dirname $0`/../images/$1.img"
